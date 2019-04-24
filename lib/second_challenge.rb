d5723c7361afa7d1f2c3b2cdfa5ec74ac0e56ead
Toni Lamont <tlam212@gmail.com>
2019-04-24 16:03:32 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch:{}
-      matirarch:{}
-      hero:{}
+      patriarch:{},
+      matirarch:{},
+      hero:{},
       hero_friends:[]
       #your key/value pairs here!
       }, 
     :capulet => {
       #your key/value pairs here!
-      patriarch:{}
-      matirarch:{}
-      heroine:{}
+      patriarch:{},
+      matirarch:{},
+      heroine:{},
       heroine_friends:[]
       }}
 
