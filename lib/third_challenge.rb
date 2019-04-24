f344fef1f5afc2a8eca46a323baf017fadbb3eb0
Toni Lamont <tlam212@gmail.com>
2019-04-24 16:05:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "lord Capulet",
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
